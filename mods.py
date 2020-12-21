aff615e92dc4f2ab6e0d2d45e5d08ba92cc7d698
WARNEVERCHANGE <ceckinkonstantin@gmail.com>
2020-12-21 19:01:44 +0500

    mods

---
+++ mods.py
@@ -0,0 +1,18 @@
+f = open("text.txt")
+n = int(f.readline())
+a = []
+
+
+
+def max_sum(ar, num):
+    return a[1], a[2], a[3]
+
+# array[0 .. n -1, 0, 1]
+# int[n][2]
+
+max_sum = 10000 * 20 + 1
+a = []
+
+for i in range(n):
+    p1, p2 = map(int,f.readline().split())
+    a.append([p1, p1])
