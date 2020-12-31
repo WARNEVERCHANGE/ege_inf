180764a85a19c8d99df96743eefb75e9fe4e8d94
WARNEVERCHANGE <ceckinkonstantin@gmail.com>
2020-12-31 18:26:20 +0500

    hello

--- divisors.py
+++ divisors.py
@@ -38,14 +38,46 @@ def izi(n):
 #         num += 1
 #         print(num, i, count, int(i ** 0.5))
 
-num = 0
-st = 2943444
-fn =2943529
-for i in range(st, fn+1):
+# num = 0
+# st = 2943444
+# fn =2943529
+# for i in range(st, fn+1):
+#     count = 0
+#     for j in range(2, int(i ** 0.5) + 1):
+#         if i % j == 0:
+#             count += 1
+#     if count == 0:
+#         num += 1
+#         print(num, i)
+
+
+# st = 33333
+# fn = 55555
+# mx = 33332
+# mn = 55556
+# counter = 0
+# for i in range(st, fn+1):
+#     if (i // 10000) % 2 != 0 and ((i % 10000) // 1000 ) % 2 != 0 and ((i % 1000) // 100) % 2 == 0 and ((i % 100) // 10) % 2 != 0 and (i % 10 ) % 2 == 0 and i % 6 != 0 and i % 7 != 0 and i % 8 != 0:
+#         counter += 1
+#         if i > mx:
+#             mx = i
+#         if i < mn:
+#             mn = i
+
+# print(counter, mx - mn)
+
+st = 4986
+fn = 32599
+s = 0
+for i in range(st, fn + 1):
     count = 0
-    for j in range(2, int(i ** 0.5) + 1):
-        if i % j == 0:
-            count += 1
-    if count == 0:
-        num += 1
-        print(num, i)
+    if int(i ** 0.5) ** 2 != i:
+        for j in range(2, int(i ** 0.5) + 1):
+            if i % j == 0:
+                count += 1
+    if count == 1:
+        s += i
+
+print(s)
+
+
