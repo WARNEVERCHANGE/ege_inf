4bfff7abcd45c09d3d230cae01cb5731dc5e27b8
WARNEVERCHANGE <ceckinkonstantin@gmail.com>
2021-01-08 09:21:04 +0500

    'lol'

--- ht1.py
+++ ht1.py
@@ -13,16 +13,45 @@
 # print(b)
 
 
+#
+#a = [10, 11, 6, 4, 22, 8, 13, 8, 9, 11]
+#
+#min_sum = int(input())
+#
+#a.sort()
+#s = 0
+#i = len(a) - 1
+#
+#
+#while i >= 0 and s + a[i] <= min_sum:
+#    s += a[i]
+#    i -= 1
+#
+#print(len(a) - i)
+#
+#if s + a[i] <= min_sum or i == -1 and s < min_sum:
+#    print('ne hvataet')
+#
+#j = 0
+#while j < len(a)  and s + a[j] < min_sum:
+#    j += 1
+#
+#
+#
+#if j == len(a):
+#    print(a[0])
+#else:
+#    print(a[j])
 
-a = [10, 11, 6, 4, 22, 8, 13, 8, 9, 11]
 
-min_sum = int(input())
 
-a.sort()
-s = 0
-i = len(a) - 1
-while s + a[i] <= min_sum:
-    s += a[i]
-    i -= 1
+f = open('26-10.txt')
 
-print(len(a) - i - 1)
+size, n = map(int, f.readline())
+a = [0] * n
+
+
+for i in range(n):
+    a[i] = int(f.readline())
+
+
